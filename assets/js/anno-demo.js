5348a15a0bcfdb62ff519217f3a4ee70b26bf62e
Paul <paul@mummify.us>
2014-09-21 12:58:39 -0700

    adding save and upload availity

--- assets/js/anno-demo.js
+++ assets/js/anno-demo.js
@@ -3,7 +3,7 @@ var editor;
 var init = function() {
   
   /*
-    Set the editor
+    Setup the editor
   */
   editor = ace.edit("json");
   editor.setTheme("ace/theme/monokai");
@@ -17,6 +17,7 @@ var init = function() {
   */
   anno.addHandler('onAnnotationCreated', annotationUpdate);
   anno.addHandler('onAnnotationUpdated', annotationUpdate);
+  anno.addHandler('onAnnotationRemoved', annotationUpdate);
 
   /*
     When the window resizes call save so the annotation
@@ -29,13 +30,17 @@ var init = function() {
   */
   document.getElementById('save').addEventListener("click", save);
 
+  /*
+    Bind the upload form
+  */
+  document.getElementById('fileForm').addEventListener('submit', upload);
 }
 
 
 /*
   Called when an annotation is saved or updated
 */
-var annotationUpdate =  function(annotation) {
+var annotationUpdate = function(annotation) {
 
   /*
     Retrieve the annotation objects from annotorious
@@ -64,11 +69,20 @@ var annotationUpdate =  function(annotation) {
     Write the output to the editor
   */
   editor.setValue(JSON.stringify(output, null, 2));
+
+  /*
+    Make the JSON available for download
+  */
+  document.getElementById('download').href = "data:text/json," + JSON.stringify(output);
+
   
 };
 
-
-var save = function(e) {
+/*
+  Called to save the JSON in the editor and load 
+  annotations into annotorious
+*/
+var save = function() {
 
   /*
     Load json from editor
@@ -88,3 +102,63 @@ var save = function(e) {
   }
 
 };
+
+/*
+  Handles the upload form submission
+  Referenced/Thanks to: http://stackoverflow.com/questions/7346563/loading-local-json-file
+*/
+var upload = function(e) {
+
+  /*
+    Prevent the form from actually submitting
+  */
+  e.preventDefault();
+
+  /*
+    Make sure our browser supports this feature
+  */
+  if(typeof window.FileReader !== 'function') {
+    alert("The file API isn't supported on this browser yet.");
+    return;
+  }
+
+  /*
+    Read the file contents, and callback to 
+    uploadComplete() when done
+  */
+  var input = document.getElementById('fileUpload');
+  file = input.files[0];
+  fr = new FileReader();
+  fr.onload = uploadComplete;
+  fr.readAsText(file);
+
+};
+
+/*
+  Handles the file upload read completion
+*/
+var uploadComplete = function(e) {
+  /*
+    Parse the JSON so we can pretty format it for
+    the editor
+  */
+  var json = JSON.parse(e.target.result);
+
+  /*
+    Place the pretty json in the editor
+  */
+  editor.setValue(JSON.stringify(json, null, 2));
+
+  // These two calls below are bit redundant
+
+  /*
+    Call save to load the editor's json into annotorious
+  */
+  save();
+
+  /*
+    Call annotation update to it's available for download
+  */  
+  annotationUpdate();
+
+}
